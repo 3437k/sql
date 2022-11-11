0821709098d1c93d6eabb19af3cc9c1df07b4abf
Kwon Hyukjin <117734137+hyukjin3437@users.noreply.github.com>
2022-11-11 09:27:39 +0900

    add : Coalesce

--- sql.sql
+++ sql.sql
@@ -1,3 +1,4 @@
+-- 2022-11-10 (Thu)
 SELECT 
   'YYYY-MM-DD HH24:MI:SS'
   , 'YYYY-MM-DD HH24:MI:00'
@@ -7,5 +8,9 @@ SELECT
 
 
 RAISE NOTICE 'from = %', from;
-  
+
+-- 2022-11-11 (Fri)
 SELECT CASE WHEN 1=1 THEN 1 ELSE 2 END AS result;  
+
+SELECT COALESCE(1, 2); -- 1
+SELECT COALESCE(NULL, 3); -- 3
